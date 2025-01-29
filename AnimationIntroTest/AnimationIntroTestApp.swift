492a501f92efc05d933b6d04793a32637d3562e2
Tom Bum Su Choi <qjatn0545@naver.com>
2025-01-29 16:04:02 +0900

    Initial Commit

---
+++ AnimationIntroTest/AnimationIntroTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationIntroTestApp.swift
+//  AnimationIntroTest
+//
+//  Created by 최범수 on 2025-01-29.
+//
+
+import SwiftUI
+
+@main
+struct AnimationIntroTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
